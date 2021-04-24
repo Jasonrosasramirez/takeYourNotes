1044e7e0448d0c890546b82ced38356e6b487f29
Jason <jrosasrjf@gmail.com>
2021-04-24 12:02:27 -0500

    started apiRoutes file with tutor

--- Develop/db/logClasses.js
+++ Develop/db/logClasses.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const util = require("util");
 const shortid = require('shortid'); // this package will be used to make unique IDs https://www.npmjs.com/package/shortid
- 
+
 
 const writeFile = util.promisify(fs.writeFile); // do not use sync version. This is asynchronous. Using sync ruins the promise :(  
 const readFile = util.promisify(fs.readFile);
@@ -16,10 +16,32 @@ class Log {
 
     read() {
 
-        return readFile("db/db.json", "utf8");
-
+        return readFile("db/db.json", "utf8"); // Encodes what was just written to the UTF-8 format. 
+        // (what we want to read, what we want to encode to)
     }
 
+    getNotes() { 
+        
+        return this.read().then(notes => {
+
+            var storedNotesArray; // function scoped
+
+            try {
+
+                storedNotesArray = [].concat(JSON.parse(notes)); 
+
+            } catch(err) {
+
+                storedNotesArray = [];
+                
+            } 
+
+            return storedNotesArray
+
+        })
+        
+    }
 
 }
 
+module.export = new Log(); // exports the Log class file function
---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const log = require("../db/logClasses"); 
+
+
+
+
+router.get("/api/notes", (req, res) => {
+
+    //log.getNotes().then((notes) =>)
+
+    // getNotes() is coming through corrrectly for some reason. check the export on logClasses
+
+
+})
+
