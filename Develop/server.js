4067c287d913bc962c7bc0be95acf3368624522b
Jason <jrosasrjf@gmail.com>
2021-04-25 21:48:50 -0500

    set up the app.listen on the server.js

--- Develop/server.js
+++ Develop/server.js
@@ -27,3 +27,12 @@ app.use(express.static("public"));
 require("./routes/apiRoutes")(app);
     //  Routing refers to how an application’s endpoints (URIs) respond to client requests. http://expressjs.com/en/guide/routing.html#route-paths
 require("./routes/htmlRoutes")(app);
+
+
+/*  Listener */
+
+app.listen(PORT, () => {
+
+    console.log("This is your express.js app speaking.\nThe express app is listening on PORT: " + PORT);
+
+}); 
