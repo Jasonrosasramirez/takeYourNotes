cbaacf308a3c445249bc89ae36c0082a38209d9d
Jason <jrosasrjf@gmail.com>
2021-04-26 19:55:15 -0500

    set the default router

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -9,3 +9,12 @@ router.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html")); 
 
 }); 
+
+router.get("*", (req, res) => {
+    // if no routes match, then the default is set to home. 
+
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+
+});
+
+module.exports = router;
